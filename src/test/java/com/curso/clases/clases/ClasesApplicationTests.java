31067dc58d6743a99f7b2a571c685222c2dcf5ee
cfgarzon <cgfarzon@outlook.com>
2019-06-01 15:30:50 -0500

    Mi primer commit

---
+++ src/test/java/com/curso/clases/clases/ClasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.clases.clases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
